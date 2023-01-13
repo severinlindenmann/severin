9ce4689dec200b1b01ac70dea9def5ac4f64bb04
Severin <github@severin.io>
2023-01-13 16:44:11 +0100

    add

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.js",
+  images: {
+    unoptimized: true,
+  },
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
-}
+};
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra(nextConfig);

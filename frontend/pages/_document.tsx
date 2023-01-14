3b1ef23ca1a21e019abe91bd3934d8373ca1941c
Severin <github@severin.io>
2023-01-14 08:10:20 +0100

    Update _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    title: "Severin.io | Blog",
+    description: "A Blog with small projects",
+    // image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
   };
 
   return (
@@ -15,12 +15,12 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
+        {/* <meta property="og:image" content={meta.image} /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@yourname" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+        {/* <meta name="twitter:image" content={meta.image} /> */}
       </Head>
       <body>
         <Main />
